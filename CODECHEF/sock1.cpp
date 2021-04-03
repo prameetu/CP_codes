e930b94a4e97607aa9889fa4dc0bfd3e4b1fc4f0
Prameet Upadhyay <prameetu20@gmail.com>
2021-04-03 23:33:11 +0530

    Add files via upload

---
+++ CODECHEF/sock1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    int cnt(0);
+    cin >> a >> b >> c;
+
+    if(a==b || b==c || a==c)
+        cout << "YES";
+    else
+        cout << "NO";
+
+
+
+}
