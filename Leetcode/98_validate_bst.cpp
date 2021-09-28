d30b72c17ae36250253621167eb091a7db0286fe
prameetu <prameetu20@gmail.com>
2021-09-28 23:56:35 +0530

    bst

---
+++ Leetcode/98_validate_bst.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    void inorder(TreeNode *root,vector <int> &res)
+    {
+        if(root)
+        {
+            inorder(root->left,res);
+            res.push_back(root->val);
+            inorder(root->right,res);
+        }
+    }
+    bool isValidBST(TreeNode *root)
+    {
+        vector <int> ans;
+        inorder(root,ans);
+        for(int i=0;i<ans.size()-1;i++)
+        {
+            if(ans[i] > ans[i+1])
+                return false;
+        }
+        return true;
+    }
+}
+
+;
